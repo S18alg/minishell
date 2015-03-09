b820cfbac0d8d1802550eff7d22a49a930ee3c7c
sle-guil <sle-guil@student.42.fr>
2015-03-09 13:59:00 +0100

    parser lvl1 done, test to come

--- srcs/parse.c
+++ srcs/parse.c
@@ -6,18 +6,45 @@
 /*   By: sle-guil <sle-guil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/02/21 17:45:50 by sle-guil          #+#    #+#             */
-/*   Updated: 2015/03/05 17:43:06 by sle-guil         ###   ########.fr       */
+/*   Updated: 2015/03/09 13:25:19 by sle-guil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-#include <stdio.h>
+
+/*
+ *	@name:	command parser
+ *	@brief:	First parser to apply on the user input, del useless space
+ *			and split each sub commande ';'
+ */
+
+static void	st_iter(char *s)
+{
+	while (*s)
+	{
+		*s = *(s + 1);
+		s++;
+	}
+}
+
+static void	st_delspace(char *s)
+{
+	while (*s)
+	{
+		if (*s == '\t' || *s == '\n')
+			*s = ' ';
+		if (ft_isspace(*s) && (ft_isspace(*(s + 1)) || !*(s + 1)))
+			st_iter(s);
+		else
+			s++;
+	}
+}
 
 char	**parse(char *cmd)
 {
 	char	**new;
 
-	// delete useless space here
+	st_delspace(cmd);
 	new = ft_strsplit(cmd, ';');
 	return (new);
 }
