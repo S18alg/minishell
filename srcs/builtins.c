e795b24c0af7e00506a2b86d3dbf9a48d693995d
sle-guil <sle-guil@student.42.fr>
2015-03-09 18:46:21 +0100

    Builtin : change the behaviors of exit

--- srcs/builtins.c
+++ srcs/builtins.c
@@ -6,7 +6,7 @@
 /*   By: sle-guil <sle-guil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/02/25 14:03:01 by sle-guil          #+#    #+#             */
-/*   Updated: 2015/03/05 18:39:57 by sle-guil         ###   ########.fr       */
+/*   Updated: 2015/03/09 14:22:35 by sle-guil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,6 @@ int		builtins(char const *cmd, char **env)
 {
 	(void)env;
 	if (!ft_strcmp(cmd, "exit"))
-		return (0);
+		exit(0);
 	return (1);
 }
