db54aa352589cfde5d9ba6e4117eab1f555bcbec
sle-guil <sle-guil@student.42.fr>
2015-03-10 17:28:48 +0100

    cd update

--- srcs/bi_cd.c
+++ srcs/bi_cd.c
@@ -6,7 +6,7 @@
 /*   By: sle-guil <sle-guil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/03/10 14:19:38 by sle-guil          #+#    #+#             */
-/*   Updated: 2015/03/10 17:19:40 by sle-guil         ###   ########.fr       */
+/*   Updated: 2015/03/10 17:28:26 by sle-guil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,5 +24,5 @@ void	bi_cd(char **env, char *cmd)
 		cmd++;
 	home = getenv(env, "HOME");
 	pwd = get_cpyenv(env, "PWD");
-	tmp = ft_strchr(*env, '=') + 1;
+	tmp = ft_strchr(home, '=') + 1;
 }
