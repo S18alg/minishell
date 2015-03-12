bd42b02b0c7a71858317ab44069767ef397fb883
sle-guil <sle-guil@student.42.fr>
2015-03-12 16:16:16 +0100

    bugfix : $>ls use to crash will searching for ls have to check if the cmd contain the dir now

--- srcs/parse_path.c
+++ srcs/parse_path.c
@@ -6,7 +6,7 @@
 /*   By: sle-guil <sle-guil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/03/11 16:14:50 by sle-guil          #+#    #+#             */
-/*   Updated: 2015/03/11 17:09:19 by sle-guil         ###   ########.fr       */
+/*   Updated: 2015/03/12 15:46:00 by sle-guil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@
  * @name :		parse_path
  * @brief :		parse the PATH variable into a char** for each path.
  */
-
 static char	*st_free(char **t, int i)
 {
 	char	*ret;
@@ -29,20 +28,36 @@ static char	*st_free(char **t, int i)
 	return (ret);
 }
 
-char		*parse_path(char const *path, char const *cmd)
+static char	**st_initdir(char const *path, char const *cmd)
+{
+	if (*cmd == '/' || *cmd == '.')
+		return (ft_strsplit("", ':'));
+	else
+		return (ft_strsplit(path, ':'));
+}
+
+static char	*st_forge(char const *path, char const *cmd)
 {
 	char	*new;
-	char	*tmp;
+
+	new = ft_strjoin(path, "/");
+	new = ft_strmerge_buff(new, cmd);
+	return (new);
+}
+
+char		*parse_path(char const *path, char const *cmd)
+{
 	char	**dir;
+	char	*new;
+	char	*tmp;
 	int		i;
 
 	i = 0;
-	dir = ft_strsplit(path, ':');
-	ft_putendl(*dir);
+	dir = st_initdir(path, cmd);
 	tmp = ft_strcdup(cmd, ' ');
 	while (dir[i])
 	{
-		new = ft_strjoin(dir[i], tmp);
+		new = st_forge(dir[i], tmp);
 		if (testpath(new) >= 8)
 			break ;
 		free(new);
