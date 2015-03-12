00e8765b3a2db414e655021fadf86f420c11d4a5
sle-guil <sle-guil@student.42.fr>
2015-03-12 16:14:40 +0100

    Check dir have to be done

--- srcs/testpath.c
+++ srcs/testpath.c
@@ -6,20 +6,17 @@
 /*   By: sle-guil <sle-guil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/03/11 16:25:34 by sle-guil          #+#    #+#             */
-/*   Updated: 2015/03/11 16:48:51 by sle-guil         ###   ########.fr       */
+/*   Updated: 2015/03/12 16:00:13 by sle-guil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int		testpath(char *path)
+int		testpath(char const *path)
 {
 	int		ret;
 
-	ft_putendl("boobs");
-	ret = (access(path, F_OK)) ? 1 : 0;
-	ret += (access(path, R_OK)) ? 2 : 0;
-	ret += (access(path, W_OK)) ? 4 : 0;
-	ret += (access(path, X_OK)) ? 8 : 0;
+	ret = (access(path, F_OK) != -1) ? 1 : 0;
+	ret += (access(path, X_OK) != -1) ? 8 : 0;
 	return (ret);
 }
