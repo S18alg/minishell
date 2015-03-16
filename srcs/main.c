feb6bfba9a79b1bae255ecda9daa65e6c91111db
sle-guil <sle-guil@student.42.fr>
2015-03-16 18:23:15 +0100

    ENV cpy

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: sle-guil <sle-guil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/02/21 15:47:56 by sle-guil          #+#    #+#             */
-/*   Updated: 2015/03/12 15:10:48 by sle-guil         ###   ########.fr       */
+/*   Updated: 2015/03/16 17:23:32 by sle-guil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,17 @@
 
 int		main(int ac, char **av, char **env)
 {
+	char	**loc_env;
 	int		i;
 
 	(void)av;
+	loc_env = env_cp(env);
 	i = 1;
 	if (ac == 1)
 	{
-		print_prompt(env);
-		while (interpreter(env) > 0)
-			print_prompt(env);
+		print_prompt(loc_env);
+		while (interpreter(loc_env) > 0)
+			print_prompt(loc_env);
 	}
 	else
 	{
