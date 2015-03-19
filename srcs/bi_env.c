39d35cf9a4eb53bf59baf657d3080651aca57325
S18alg <suliac186@hotmail.fr>
2015-03-19 23:11:54 +0100

    WIP

--- srcs/bi_env.c
+++ srcs/bi_env.c
@@ -14,15 +14,15 @@
 
 /*!
  *	@name :		bi_env
- *	@brief :	Print the environment variables
+ *	@brief :	Print the environment variables or execute a commande with custom env.
  */
-static char	**st_initenv(char **env, char *arg)
+static char	**st_initenv(char **env, char const *arg)
 {
 	char	**new;
 
 	if (*arg == '-')
 	{
-		if (*(arg + 1) == 'i' && *(arg + 2) == ' ')
+		if (*(arg + 1) == 'i')
 		{
 			new = malloc (sizeof(char*) * 2);
 			*new = ft_strdup(ft_strchr(arg, ' ') + 1);
@@ -50,9 +50,11 @@ static void	st_print(char **env)
 
 int		bi_env(char **env, char const *arg)
 {
-	char	*cmd;
+	//char	*cmd;
 	int		ret;
 
+	ret = 0;
+	(void)arg;
 	if (!(env = st_initenv(env, arg)))
 		return (-1);
 	st_print(env);
