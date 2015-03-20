2fb62a82d02efe3eb9feb202d38daf9f48832ce6
S18alg <suliac186@hotmail.fr>
2015-03-20 19:38:38 +0100

    WIP

--- srcs/bi_env.c
+++ srcs/bi_env.c
@@ -16,29 +16,6 @@
  *	@name :		bi_env
  *	@brief :	Print the environment variables or execute a commande with custom env.
  */
-static char	**st_initenv(char **env, char const *arg)
-{
-	char	**new;
-
-	if (*arg == '-')
-	{
-		if (*(arg + 1) == 'i')
-		{
-			new = malloc (sizeof(char*) * 2);
-			*new = ft_strdup(ft_strchr(arg, ' ') + 1);
-			*(new + 1) = NULL;
-			return (new);
-		}
-		else
-		{
-			ft_putendl("env: Incorrect option. use -i to ignore env");
-			return (NULL);
-		}
-	}
-	else
-		return (env_cp(env));
-}
-
 static void	st_print(char **env)
 {
 	while (*env)
@@ -50,14 +27,18 @@ static void	st_print(char **env)
 
 int		bi_env(char **env, char const *arg)
 {
-	//char	*cmd;
+	char	*cmd;
+	char	*tmp;
 	int		ret;
 
 	ret = 0;
-	(void)arg;
-	if (!(env = st_initenv(env, arg)))
-		return (-1);
-	st_print(env);
-	env_free(env);
+	cmd = (arg) ? ft_strchr(arg, ' ') + 1 : NULL;
+	tmp = (arg) ? ft_strcdup(arg, ' ') : NULL;
+	while (cmd && ft_strchr(cmd, '='))
+		cmd = ft_strchr(cmd, ' ') + 1;
+	if (!cmd)
+		st_print(env);
+	else
+		runcmd(cmd, env);
 	return (ret);
 }
