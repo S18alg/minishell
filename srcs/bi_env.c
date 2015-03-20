3eca8507c101091548e6958727ba5ef42fbd592b
S18alg <suliac186@hotmail.fr>
2015-03-21 00:54:31 +0100

    bi_env, done and tested

--- srcs/bi_env.c
+++ srcs/bi_env.c
@@ -25,20 +25,63 @@ static void	st_print(char **env)
 	}
 }
 
-int		bi_env(char **env, char const *arg)
+static int	st_over(char const *s)
 {
-	char	*cmd;
-	char	*tmp;
-	int		ret;
+	if (!s)
+		return (0);
+	if (s[0] == '-')
+	{
+		if (s[1] == 'i' && (!s[2] || s[2] == ' '))
+			return (1);
+		else
+		{
+			ft_putendl("incorrect use of env : us -i to ignor current env.");
+			return (-1);
+		}
+	}
+	return (0);
+}
 
-	ret = 0;
-	cmd = (arg) ? ft_strchr(arg, ' ') + 1 : NULL;
-	tmp = (arg) ? ft_strcdup(arg, ' ') : NULL;
-	while (cmd && ft_strchr(cmd, '='))
-		cmd = ft_strchr(cmd, ' ') + 1;
+static char const	*st_init(char ***env, char const *cmd)
+{
+	char	*tmp;
+	char	*c;
+	
+	while (cmd && ft_strchr(cmd, '=') && (tmp = ft_strcdup(cmd, ' ')))
+	{
+		if ((c = ft_strchr(tmp, '=')))
+		{
+			*c = ' ';
+			bi_setenv(*env, tmp, 1);
+			*env = env_refresh(*env);
+		}
+		free(tmp);
+		cmd = (ft_strchr(cmd, ' ')) ? ft_strchr(cmd, ' ') + 1 : NULL;
+	}
+	return (cmd);
+}
+
+int			bi_env(char **env, char const *arg)
+{
+	char const	*cmd;
+	int			ret;
+
+	if (arg)
+		arg++;
+	if ((ret = st_over(arg)) > 0)
+	{
+		env = env_cp(NULL);
+		arg = (ft_strchr(arg, ' ')) ? ft_strchr(arg, ' ') + 1 : NULL;
+	}
+	else if (!ret)
+		env = env_cp(env);
+	else
+		return (-1);
+	cmd = st_init(&env, arg);
 	if (!cmd)
 		st_print(env);
 	else
 		runcmd(cmd, env);
-	return (ret);
+	env_free(env);
+	return (0);
 }
