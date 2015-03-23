ac64126301629bf36114df56a41c98dbf3f08f05
sle-guil <sle-guil@student.42.fr>
2015-03-23 14:43:24 +0100

    bug fix: no = allow anymore

--- srcs/bi_setenv.c
+++ srcs/bi_setenv.c
@@ -6,7 +6,7 @@
 /*   By: sle-guil <sle-guil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/03/16 17:27:50 by sle-guil          #+#    #+#             */
-/*   Updated: 2015/03/18 15:47:18 by sle-guil         ###   ########.fr       */
+/*   Updated: 2015/03/23 14:38:19 by sle-guil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,12 @@ static char	*st_init(char const *s)
 
 	new = ft_strdup(s);
 	tmp = ft_strchr(new, ' ');
-	if (!tmp || tmp != ft_strrchr(new, ' '))
+	if (!tmp || tmp != ft_strrchr(new, ' ') || ft_strchr(new, '='))
 	{
-		ft_putendl_fd("setenv : incorrect number of arg", 2);
+		if (ft_strchr(new, '='))
+			ft_putendl_fd("Error : Do not use \'=\' to setenv", 2);
+		else if (tmp != ft_strrchr(new, ' '))
+			ft_putendl_fd("setenv : incorrect number of arg", 2);
 		free(new);
 		return (NULL);
 	}
